9c93526534546542e0e1863ffab98e109b94ded7
amadeus-b <167602238+amadeus-b@users.noreply.github.com>
2025-03-28 18:35:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,16 +34,6 @@ export default defineConfig({
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			head: [
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image', content: ogUrl },
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image:alt', content: ogImageAlt },
-				},
-			],
 			sidebar: [
     				{
       					label: 'Guides',
